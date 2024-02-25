ff367a5c15333adadb4cde392eec09711ae8eba3
MahmoudMoheyEldeen <mahmoudmohey1992@gmail.com>
2024-02-25 21:54:35 +0200

    sda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { TableModule } from 'primeng/table';
+import { RatingModule } from 'primeng/rating';
+import { TagModule } from 'primeng/tag';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, HeaderComponent, FooterComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HeaderComponent,
+    FooterComponent,
+    ButtonModule,
+    BrowserAnimationsModule,
+    TableModule,
+    RatingModule,
+    TagModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
+  products: any[] = [
+    {
+      name: 'Dress',
+      image: "src='assets/imgs/main-slider-1.jpeg'",
+      price: 20,
+      category: 'Women Clothes',
+      reviews: 'asdas',
+      status: 'asd',
+    },
+  ];
 }
