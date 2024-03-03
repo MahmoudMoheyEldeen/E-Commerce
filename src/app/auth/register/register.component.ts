7645cc3cedeed10305755468230ff1b6bf7826c3
MahmoudMoheyEldeen <mahmoudmohey1992@gmail.com>
2024-03-03 08:54:42 +0200

    qweqe

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -11,32 +11,25 @@ import { AuthServiceService } from 'src/app/Services/auth-service.service';
 })
 export class RegisterComponent {
   registerform: FormGroup = new FormGroup({
-    name: new FormControl(),
-    email: new FormControl(),
-    password: new FormControl(),
-    rePassword: new FormControl(),
-    phone: new FormControl(),
+    name: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(25),
+    ]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(/^[a-z][a-z0-9]{6-20}$/),
+    ]),
+    rePassword: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(/^[a-z][a-z0-9]{6-20}$/),
+    ]),
+    phone: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(/^01[0125][0-9]{8}$/),
+    ]),
   });
-  // registerform: FormGroup = new FormGroup({
-  //   name: new FormControl(null, [
-  //     Validators.required,
-  //     Validators.minLength(4),
-  //     Validators.maxLength(25),
-  //   ]),
-  //   email: new FormControl(null, [Validators.required, Validators.email]),
-  //   password: new FormControl(null, [
-  //     Validators.required,
-  //     Validators.pattern(/^[a-z][a-z0-9]{6-20}$/),
-  //   ]),
-  //   rePassword: new FormControl(null, [
-  //     Validators.required,
-  //     Validators.pattern(/^[a-z][a-z0-9]{6-20}$/),
-  //   ]),
-  //   phone: new FormControl(null, [
-  //     Validators.required,
-  //     Validators.pattern(/^01[0125][0-9]{8}$/),
-  //   ]),
-  // });
   constructor(private _authService: AuthServiceService) {}
 
   submitRegister() {
