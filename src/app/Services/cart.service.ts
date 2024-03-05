ae63cc40415180e606e0c289efb17fc7d6ff4e73
MahmoudMoheyEldeen <mahmoudmohey1992@gmail.com>
2024-03-05 10:21:33 +0200

    ads

--- src/app/Services/cart.service.ts
+++ src/app/Services/cart.service.ts
@@ -7,30 +7,19 @@ import { Observable } from 'rxjs';
 })
 export class CartService {
   baseUrl: string = 'https://ecommerce.routemisr.com';
-  headers: any = {
-    // token: localStorage.getItem('usertoken' || ''),
-    token:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZTQ1ODE5YmU4YjUyMzIzNWQwNjNlMiIsIm5hbWUiOiJheWE1NjciLCJyb2xlIjoidXNlciIsImlhdCI6MTcwOTU3NDE2NCwiZXhwIjoxNzE3MzUwMTY0fQ.OCMAmFp8Mm57wBmrOUguDP-BAK-s1sZeCEICg9OG68A',
-  };
 
   constructor(private _httpClient: HttpClient) {}
 
   addProductToCart(productId: string): Observable<any> {
-    return this._httpClient.post(
-      `${this.baseUrl}/api/v1/cart`,
-      { productId: productId },
-      { headers: this.headers }
-    );
+    return this._httpClient.post(`${this.baseUrl}/api/v1/cart`, {
+      productId: productId,
+    });
   }
   getLoggedUserCart(): Observable<any> {
-    return this._httpClient.get(`${this.baseUrl}/api/v1/cart`, {
-      headers: this.headers,
-    });
+    return this._httpClient.get(`${this.baseUrl}/api/v1/cart`);
   }
 
   deleteProductToCart(productId: string): Observable<any> {
-    return this._httpClient.delete(`${this.baseUrl}/api/v1/cart/${productId}`, {
-      headers: this.headers,
-    });
+    return this._httpClient.delete(`${this.baseUrl}/api/v1/cart/${productId}`);
   }
 }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -32,7 +32,7 @@ export class LoginComponent {
     if (this.loginForm.valid) {
       this._authservice.logIn(this.loginForm.value).subscribe({
         next: (response) => {
-          console.log(response.token);
+          console.log('this is the token', response.token);
           localStorage.setItem('userToken', response.token);
           this._authservice.decodeUserToken();
           if (response.message == 'success') {
