2a1b6dd9fff2a528347e688ffeb85460c22ea477
MahmoudMoheyEldeen <mahmoudmohey1992@gmail.com>
2024-03-07 09:51:25 +0200

    asd

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,10 +12,12 @@ import { Products } from '../../interfaces/products';
 export class HomeComponent implements OnInit {
   term: string = '';
   first = 0;
+  allCategories: any[] = [];
 
   rows = 10;
   ngOnInit(): void {
     this.getAllProducts();
+    this.getAllCategories();
   }
   constructor(private _productService: ProductsService) {}
   userSelectedRating: number = 3;
@@ -31,12 +33,31 @@ export class HomeComponent implements OnInit {
   getAllProducts() {
     this._productService.getAllProducts().subscribe({
       next: (Response) => {
-        console.log(
-          'this is subcategory ',
-          Response.data[0].subcategory[0].name
-        );
         this.allProducts = Response.data;
         console.log(this.allProducts);
+      },
+    });
+  }
+  getAllCategories() {
+    // let allCategories: { id: number; name: string }[] = [];
+    this._productService.getAllCategory().subscribe({
+      next: (response) => {
+        console.log('this is all categories', response.data);
+        this.allCategories = response.data;
+        // console.log('hhhhhhhhhhhhhhhhhhhhh', response.data[0]._id);
+        // for (let i = 0; i < response.data.length; i++) {
+        //   let category = {
+        //     id: response.data[i]._id, // Fix the property name to _id
+        //     name: response.data[i].name,
+        //   };
+        //   allCategories.push(category); // Fix the array to allCategories
+        //   console.log(category);
+        // }
+        // this.allCategoriesName = allCategories.map((category) => ({
+        //   label: category.name,
+        //   value: category.id,
+        // }));
+        // console.log('new with map', this.allCategoriesName);
       },
     });
   }
